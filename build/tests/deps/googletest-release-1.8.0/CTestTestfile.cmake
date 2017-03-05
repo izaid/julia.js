46a60af3c25bfd8a54ae139e0a26fbb03162bcf9
izaid <hi@irwinzaid.com>
2017-03-05 17:35:49 +0000

    Removed build

--- build/tests/deps/googletest-release-1.8.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/irwin/git/j2/deps/googletest-release-1.8.0
-# Build directory: /Users/irwin/git/j2/build/tests/deps/googletest-release-1.8.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/tests/deps/googletest-release-1.8.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/irwin/git/j2/deps/googletest-release-1.8.0/googlemock
-# Build directory: /Users/irwin/git/j2/build/tests/deps/googletest-release-1.8.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
