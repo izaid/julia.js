e15a480d733704b43052d5763af7d6c4b3173f5f
izaid <hi@irwinzaid.com>
2017-03-06 15:50:02 +0000

    Added NAN

--- src/_v8.cpp
+++ src/_v8.cpp
@@ -2,6 +2,8 @@
 
 #include <j2.h>
 
+#include <nan.h>
+
 static v8::Platform *m_platform;
 static v8::Isolate *m_isolate;
 
@@ -39,14 +41,22 @@ void jl_v8_destroy() {
 }
 
 jl_value_t *jl_v8_eval(const char *src) {
-  v8::Isolate::Scope isolate_scope(m_isolate);
-  v8::HandleScope handle_scope(m_isolate);
-  v8::Local<v8::Context> context; // = v8::Context::New(m_isolate);
-  v8::Context::Scope context_scope(context);
+  Nan::MaybeLocal<Nan::BoundScript> script =
+      Nan::CompileScript(v8::String::NewFromUtf8(m_isolate, src));
 
-  v8::Local<v8::Script> script =
-      v8::Script::Compile(context, v8::String::NewFromUtf8(m_isolate, src))
-          .ToLocalChecked();
+  Nan::MaybeLocal<v8::Value> value = Nan::RunScript(script.ToLocalChecked());
+  return j2::FromJavaScriptValue(value.ToLocalChecked());
 
-  return j2::FromJavaScriptValue(script->Run(context).ToLocalChecked());
+  /*
+    v8::Isolate::Scope isolate_scope(m_isolate);
+    v8::HandleScope handle_scope(m_isolate);
+    v8::Local<v8::Context> context =  v8::Context::New(NULL); // =
+    v8::Context::New(m_isolate);
+    v8::Context::Scope context_scope(context);
+
+    v8::Local<v8::Script> script =
+        v8::Script::Compile(context, v8::String::NewFromUtf8(m_isolate, src))
+            .ToLocalChecked();
+
+  */
 }
